49a6bd82834a5ce7dbaf4ac17948cf5668f46c3a
mh-shovon <shovon33266@gmail.com>
2025-09-16 01:22:46 +0600

    Java :: Java abstraction class implementation

---
+++ src/Class_And_Practice/Class_07/AbstractClassTest.java
@@ -0,0 +1,9 @@
+package Class_And_Practice.Class_07;
+
+public class AbstractClassTest {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+        myCar.start();
+        myCar.stop();
+    }
+}
---
+++ src/Class_And_Practice/Class_07/Car.java
@@ -0,0 +1,11 @@
+package Class_And_Practice.Class_07;
+
+public class Car extends Vehicle {
+
+    @Override
+    public void start() {
+        System.out.println("Starting Car");
+    }
+
+
+}
