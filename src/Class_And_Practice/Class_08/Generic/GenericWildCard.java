52d8e9f87bc975dd2526b06cc6facefa7a0f6a93
mh-shovon <shovon33266@gmail.com>
2025-09-18 23:21:20 +0600

    Add generic and wild card classes with corresponding test implementation

--- src/Class_And_Practice/Class_08/Generic/GenericWildCard.java
+++ src/Class_And_Practice/Class_08/Generic/GenericWildCard.java
@@ -1,14 +1,13 @@
 package Class_And_Practice.Class_08.Generic;
 
-public class Generic<T> {
+public class GenericWildCard <T extends Number> {
     private T value;
 
-    public Generic(T value) {
+    public GenericWildCard(T value) {
         this.value = value;
     }
 
     public T getValue() {
         return value;
     }
-
 }
